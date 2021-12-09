6ee915a094d638d6a4e3a986d23b438646d68bd9
jose <jabelcastellanosjoo@unm.edu>
2021-12-09 15:24:31 -0700

    Tweak upload_video script.

--- upload_video.py
+++ upload_video.py
@@ -4,18 +4,35 @@
 from Google import Create_Service
 from googleapiclient.http import MediaFileUpload
 
-CLIENT_SECRET_FILE = 'client_secrets.json'
+from argparse import ArgumentParser as arg_parser
+from os.path import basename as basename
+from re import sub as sub
+
+CLIENT_SECRET_FILE = '/home/jose/Documents/GithubProjects/VideoUploaderMachine/client_secrets.json'
 API_NAME = 'youtube'
 API_VERSION = 'v3'
 SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+THUMBNAIL_PATH = '/home/jose/Pictures/thumbnail.jpg'
 
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+parser = arg_parser("simple_parser")
+# TODO: Add argument for thumbnail with THUMBNAIL_PATH as default 
+parser.add_argument(
+        "--file", 
+        help="Name of the file to be uploaded including path", 
+        action='store',
+        type=str)
+args = parser.parse_args()
+
+file_path = args.file
+file_name = basename(file_path)
+title_name = sub(r"[_-]", ' ', file_name[:file_name.find('.')])
 
 request_body = {
         'snippet': {
             'categoryId': 28,
-            # TODO: Set 'title' from external argument
-            'title': 'Test Video',
+            'title': title_name,
             'description': 'Daily grind',
             'tags': ['Studying', 'Computer Science', 'Math']
             },
@@ -26,16 +43,15 @@ request_body = {
         'notifySubscribers': False
         }
 
-# TODO: Set media file from external argument
-mediaFile = MediaFileUpload('/home/jose/Videos/ScreenCasts/2021-12-08_08-34-16.mkv')
+media_file = MediaFileUpload(file_name)
 
 response_upload = service.videos().insert(
         part='snippet,status',
         body=request_body,
-        media_body=mediaFile
+        media_body=media_file
         ).execute()
 
 service.thumbnails().set(
         videoId=response_upload.get('id'),
-        media_body=MediaFileUpload('/home/jose/Pictures/thumbnail.jpg')
+        media_body=MediaFileUpload(THUMBNAIL_PATH)
         ).execute()
